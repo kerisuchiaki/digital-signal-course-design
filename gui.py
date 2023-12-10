2af1d53c9b18674891234178bf4f459578c6a08c
kerisuchiaki <15117846003@163.com>
2023-12-10 14:14:12 +0800

    修复一下打开图片BUG

--- gui.py
+++ gui.py
@@ -20,14 +20,31 @@ class ImageProcessorApp:
     def load_image(self):
         file_path = filedialog.askopenfilename()
         self.image = Image.open(file_path)
-        self.photo = ImageTk.PhotoImage(self.image)
-        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
+        self.display_image()
 
     def process_image(self):
-        # 在这里添加图像处理功能的代码，例如裁剪、调整亮度、对比度等
+        if hasattr(self, 'image'):
+            # Example: Cropping (crop the image to a square)
+            width, height = self.image.size
+            min_dim = min(width, height)
+            cropped_image = self.image.crop((0, 0, min_dim, min_dim))
 
-        # 更新处理后的图片到界面上
-        self.photo = ImageTk.PhotoImage(self.image)
+            # Example: Adjusting brightness
+            brightness_factor = 1.5
+            brightness_adjusted = cropped_image.point(lambda p: p * brightness_factor)
+
+            # Example: Adjusting contrast
+            contrast_factor = 1.5
+            contrast_adjusted = brightness_adjusted.point(lambda p: (p - 128) * contrast_factor + 128)
+
+            # Display the processed image
+            self.image = contrast_adjusted
+            self.display_image()
+
+    def display_image(self):
+        # Resize image to fit in the canvas
+        resized_image = self.image.resize((400, 400), Image.ANTIALIAS)
+        self.photo = ImageTk.PhotoImage(resized_image)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
 
 
